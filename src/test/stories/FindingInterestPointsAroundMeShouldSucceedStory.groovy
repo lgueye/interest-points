7a316be468dd3021818ce5f565fa4dab08e8e5a7
louis gueye <louis.gueye@gmail.com>
2011-09-26 16:20:30 +0200

    parameterize easyb tests, generate html report

--- src/test/stories/FindingInterestPointsAroundMeShouldSucceedStory.groovy
+++ src/test/stories/FindingInterestPointsAroundMeShouldSucceedStory.groovy
@@ -12,7 +12,6 @@ import org.springframework.web.client.RestTemplate
 
 scenario "finding interest points around me should succeed" , {
 
-    def location
     def HttpHeaders headers = new HttpHeaders()
     def uri
     def ctx = new ClassPathXmlApplicationContext("classpath:stories-context.xml")
@@ -20,15 +19,26 @@ scenario "finding interest points around me should succeed" , {
     def ResponseEntity<List<InterestPoint>> responseEntity
     def results
 
+    def streetAddress = "10%20bd%20haussmann"
+    def postalCode = "75009"
+    def city = "paris"
+    def countryCode = "fr"
+    def requestContentType = "application/x-www-form-urlencoded"
+    def responseContentType = "application/x-www-form-urlencoded"
+    def expectedResultsCount = 21
+    def expectedRestaurantsCount = 5
+    def expectedPubsCount = 9
+    def expectedSubwayStationsCount = 2
+    
     given "I am a valid system user", {}
 
-    and "I provide the location '10 bd haussmann, 75009 paris'", { location ="10 bd haussmann, 75009 paris" }
+    and "I provide the location '10 bd haussmann'", {}
     
-    and "I send 'application/x-www-form-urlencoded'", {
+    and "I send ${requestContentType}", {
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED)
     }
     
-    and "I receive 'application/json'", {
+    and "I receive ${responseContentType}", {
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON))
         headers.setAcceptCharset(Arrays.asList(MappingJacksonHttpMessageConverter.DEFAULT_CHARSET))
     }
@@ -36,13 +46,13 @@ scenario "finding interest points around me should succeed" , {
     when "I ask for interest points around that location", {
         
         StringBuilder queryString = new StringBuilder("http://localhost:9090/interest-points/find?")
-        queryString.append("address.city=paris")
+        queryString.append("address.city=${city}")
         queryString.append("&")
-        queryString.append("address.countryCode=fr")
+        queryString.append("address.countryCode=${countryCode}")
         queryString.append("&")
-        queryString.append("address.postalCode=75009")
+        queryString.append("address.postalCode=${postalCode}")
         queryString.append("&")
-        queryString.append("address.streetAddress=10%20bd%20haussmann")
+        queryString.append("address.streetAddress=${streetAddress}")
         uri = URI.create(queryString.toString())
         HttpEntity<InterestPoint> requestEntity = new HttpEntity<InterestPoint>(headers)
         responseEntity = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, List.class)
@@ -55,32 +65,32 @@ scenario "finding interest points around me should succeed" , {
         Assert.assertEquals(200, responseEntity.getStatusCode().value())
     }
 
-    and "The response should include 21 interest points", {
+    and "The response should include ${expectedResultsCount} interest points", {
         Assert.assertTrue(responseEntity.hasBody())
         Assert.assertNotNull(responseEntity.getBody())
         results = responseEntity.getBody()
-        Assert.assertEquals(21, results.size())
+        Assert.assertEquals(expectedResultsCount, results.size())
     }
 
-    and "The response should include 5 restaurants", {
+    and "The response should include ${expectedRestaurantsCount} restaurants", {
         Assert.assertTrue(responseEntity.hasBody())
         Assert.assertNotNull(responseEntity.getBody())
         results = responseEntity.getBody()
-        Assert.assertEquals(5, results. findAll {it.type == "RESTAURANT" }.size())
+        Assert.assertEquals(expectedRestaurantsCount, results. findAll {it.type == "RESTAURANT" }.size())
     }
     
-    and "The response should include 9 pubs", {
+    and "The response should include ${expectedPubsCount} pubs", {
         Assert.assertTrue(responseEntity.hasBody())
         Assert.assertNotNull(responseEntity.getBody())
         results = responseEntity.getBody()
-        Assert.assertEquals(9, results. findAll {it.type == "PUB" }.size())
+        Assert.assertEquals(expectedPubsCount, results. findAll {it.type == "PUB" }.size())
     }
     
-    and "The response should include 2 subway stations", {
+    and "The response should include ${expectedSubwayStationsCount} subway stations", {
         Assert.assertTrue(responseEntity.hasBody())
         Assert.assertNotNull(responseEntity.getBody())
         results = responseEntity.getBody()
-        Assert.assertEquals(2, results. findAll {it.type == "SUBWAY_STATION" }.size())
+        Assert.assertEquals(expectedSubwayStationsCount, results. findAll {it.type == "SUBWAY_STATION" }.size())
     }
     
     
